5eff0d2cbf32d6b982c786f3ff95486ff3e15430
Travis <travis.witt13@gmail.com>
2022-06-26 13:47:32 -0500

    created page-template and moved generate page into separate js file

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-};
+const generatePage = require('./src/page-template.js');
 
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
